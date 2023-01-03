9a716f9ddb9af71dcdd8fc0ae8a1c98e5526bd3c
Krishna <krishnabiswakarmasc@gmail.com>
2023-01-03 12:53:45 +0530

    Added cross button to the modal

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import ExpenseForm from "./ExpenseForm"
 import Card from "../UI/Card"
 import './NewExpense.css'
+import crossImage from '../../Assets/cross-button.png'
 
 const NewExpense = (props) => {
     const [addExpense, setAddExpense] = useState(false)
@@ -18,8 +19,14 @@ const NewExpense = (props) => {
     const onSubmitHandler = () => {
         setAddExpense(false);
     }
+    const closeModal = () => {
+        setAddExpense(false);
+    }
     return (
         <Card className="newExpense">
+            {addExpense ? <div className="image_container">
+                <img className="crossImage" src={crossImage} alt="Close Button" onClick={closeModal} />
+            </div> : null}
             {!addExpense ? <button className="newExpense__button" onClick={addExpenseHandler} >Add New Expense</button> : null}
             {addExpense ? <ExpenseForm onSaveExpenseForm={getDataFromExpenseForm} addNewExpense={onSubmitHandler} /> : null}
         </Card>
